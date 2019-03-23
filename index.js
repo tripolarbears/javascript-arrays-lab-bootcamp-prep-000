56782ed42c63a62602c77f049356fa2d649425cf
fearless-snippet-5362 <cpk1115@gmail.com>
2019-03-23 19:56:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivleyAppendKitten (name) {
-  
+  return [kittens, ...name]
 }
